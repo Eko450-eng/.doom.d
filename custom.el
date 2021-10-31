11b9a87c5de31b5d97b452d923117291e029a154
Eko450-eng <ekrem@wipdesign.de>
2021-10-31 20:05:03 +0100

    Quicksave before changing to arch

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(gdscript-godot-executable "~/Desktop/Godot_v3.3.3-stable_x11.64")
  '(package-selected-packages
-   '(vterm hide-mode-line prettier htmltagwrap company tide rjsx-mode evil treemacs)))
+   '(ipretty prettier-js vterm hide-mode-line prettier htmltagwrap company tide rjsx-mode evil treemacs)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -16,8 +16,7 @@
 (global-set-key (kbd "M-h") 'evil-window-left)
 (global-set-key (kbd "M-j") 'evil-window-down)
 (global-set-key (kbd "M-k") 'evil-window-up)
-(defun run-py ()
-  (interactive)
-  (shell-command (concat "python3 " buffer-file-name)))
-(global-set-key (kbd "M-c") 'run-py)
-
+;; (defun run-py ()
+;;   (interactive)
+;;   (shell-command (concat "python3 " buffer-file-name)))
+;; (global-set-key (kbd "M-c") 'run-py)
