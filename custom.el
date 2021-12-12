978b1ec2df19c1f34d54a6223c102a0f8dcc94a6
Eko450-eng <ekrem@wipdesign.de>
2021-12-12 20:37:11 +0100

    Added java environment and gotham theme

--- custom.el
+++ custom.el
@@ -1,9 +1,34 @@
-(custom-set-faces
+(custom-set-faces)
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ 
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
@@ -113,7 +138,8 @@
 (global-set-key (kbd "M-h") 'evil-window-left)
 (global-set-key (kbd "M-j") 'evil-window-down)
 (global-set-key (kbd "M-k") 'evil-window-up)
-(global-set-key (kbd "s-c") '+vterm/toggle)
+(global-set-key (kbd "s-x") '+vterm/toggle)
+(global-set-key (kbd "s-g") 'shell-command (./gradlew runClient))
 
 (setq auto-mode-alist (delete '("\\.ts\\'" . typescript-tsx-mode) auto-mode-alist))
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . rjsx-mode))
@@ -130,8 +156,44 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#1e1e1e" "#D16969" "#579C4C" "#D7BA7D" "#339CDB" "#C586C0" "#85DDFF" "#d4d4d4"])
+ '(custom-safe-themes
+   '("06989bc1e34d6b52be32c817c3a0710f5e740bcd006376a8faf9352200e4051d" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" default))
+ '(exwm-floating-border-color "#252526")
+ '(fci-rule-color "#37474F")
+ '(highlight-tail-colors ((("#232a22" "#232a21") . 0) (("#283134" "#243034") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#171F24" "#237AD3"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#171F24" "#579C4C"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#171F24" "#777778"))
+ '(objed-cursor-color "#D16969")
  '(package-selected-packages
-   '(org-preview-html skewer-mode centered-cursor-mode vterm tide shell-pop rjsx-mode prettier-js prettier lsp-python-ms lsp-intellij lorem-ipsum jtags ipretty intellij-theme htmltagwrap hide-mode-line gradle-mode gotham-theme flymake-gradle flycheck-gradle evil company)))
+   '(gotham-theme multiple-cursors org-preview-html skewer-mode vterm tide shell-pop rjsx-mode prettier-js prettier lsp-python-ms lsp-intellij lorem-ipsum jtags ipretty intellij-theme htmltagwrap hide-mode-line evil company))
+ '(pdf-view-midnight-colors (cons "#d4d4d4" "#1e1e1e"))
+ '(rustic-ansi-faces
+   ["#1e1e1e" "#D16969" "#579C4C" "#D7BA7D" "#339CDB" "#C586C0" "#85DDFF" "#d4d4d4"])
+ '(vc-annotate-background "#1e1e1e")
+ '(vc-annotate-color-map
+    (list
+         (cons 20 "#579C4C")
+         (cons 40 "#81a65c")
+         (cons 60 "#acb06c")
+         (cons 80 "#D7BA7D")
+         (cons 100 "#d8ab79")
+         (cons 120 "#d99c76")
+         (cons 140 "#DB8E73")
+         (cons 160 "#d38b8c")
+         (cons 180 "#cc88a6")
+         (cons 200 "#C586C0")
+         (cons 220 "#c97ca3")
+         (cons 240 "#cd7286")
+         (cons 260 "#D16969")
+         (cons 280 "#ba6c6c")
+         (cons 300 "#a37070")
+         (cons 320 "#8d7374")
+         (cons 340 "#37474F")
+         (cons 360 "#37474F")))
+ '(vc-annotate-very-old-color nil))
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
@@ -153,7 +215,7 @@
  '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
  '(objed-cursor-color "#ff6c6b")
  '(package-selected-packages
-   '(centered-cursor-mode jtags javadoc-lookup javap-mode intellij-theme flymake-gradle lsp-java flycheck-gradle gradle-mode lsp-intellij lorem-ipsum rjsx-mode typescript-mode centered-window lsp-python-ms gotham-theme shell-pop ipretty prettier-js vterm hide-mode-line prettier htmltagwrap company tide evil treemacs))
+   '(jtags javadoc-lookup javap-mode intellij-theme flymake-gradle lsp-java flycheck-gradle gradle-mode lsp-intellij lorem-ipsum rjsx-mode typescript-mode centered-window lsp-python-ms gotham-theme shell-pop ipretty prettier-js vterm hide-mode-line prettier htmltagwrap company tide evil treemacs))
  '(pdf-view-midnight-colors (cons "#bbc2cf" "#282c34"))
  '(rustic-ansi-faces
    ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
@@ -179,3 +241,6 @@
     (cons 340 "#5B6268")
     (cons 360 "#5B6268")))
  '(vc-annotate-very-old-color nil)
+
+(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
+(load-theme 'gotham)
