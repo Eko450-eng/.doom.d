d269e402b9f363a7a8b10d3cf259f36c0b2422e8
Eko450-eng <ekrem@wipdesign.de>
2021-12-13 19:39:00 +0100

    Added lsp autostarts

--- custom.el
+++ custom.el
@@ -134,15 +134,38 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+
+
 (global-set-key (kbd "M-l") 'evil-window-right)
 (global-set-key (kbd "M-h") 'evil-window-left)
 (global-set-key (kbd "M-j") 'evil-window-down)
 (global-set-key (kbd "M-k") 'evil-window-up)
 (global-set-key (kbd "s-x") '+vterm/toggle)
-(global-set-key (kbd "s-g") 'shell-command (./gradlew runClient))
+(global-set-key (kbd "C-x C-u") 'lsp-java-add-import)
 
 (setq auto-mode-alist (delete '("\\.ts\\'" . typescript-tsx-mode) auto-mode-alist))
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . rjsx-mode))
+(add-to-list 'auto-mode-alist '("\\.java\\'" . java-mode))
+
+(defun forgeRunClient()
+  (interactive)
+  (let ((default-directory "/home/eko/code/minecraft/forge-1.16.5-36.2.19-mdk"))(shell-command "./gradlew runClient")))
+
+(global-set-key (kbd "M-c") 'forgeRunClient)
 
 
 ;; (define-key global-map [?\s-c] '+vterm/toggle)
@@ -157,42 +180,46 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
-   ["#1e1e1e" "#D16969" "#579C4C" "#D7BA7D" "#339CDB" "#C586C0" "#85DDFF" "#d4d4d4"])
+   ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
  '(custom-safe-themes
-   '("06989bc1e34d6b52be32c817c3a0710f5e740bcd006376a8faf9352200e4051d" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" default))
- '(exwm-floating-border-color "#252526")
- '(fci-rule-color "#37474F")
- '(highlight-tail-colors ((("#232a22" "#232a21") . 0) (("#283134" "#243034") . 20)))
- '(jdee-db-active-breakpoint-face-colors (cons "#171F24" "#237AD3"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#171F24" "#579C4C"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#171F24" "#777778"))
- '(objed-cursor-color "#D16969")
+   '("06989bc1e34d6b52be32c817c3a0710f5e740bcd006376a8faf9352200e4051d" default))
+ '(exwm-floating-border-color "#191b20")
+ '(fci-rule-color "#5B6268")
+ '(highlight-tail-colors
+   ((("#333a38" "#99bb66" "green")
+     . 0)
+    (("#2b3d48" "#46D9FF" "brightcyan")
+     . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#51afef"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#98be65"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
+ '(objed-cursor-color "#ff6c6b")
  '(package-selected-packages
-   '(gotham-theme multiple-cursors org-preview-html skewer-mode vterm tide shell-pop rjsx-mode prettier-js prettier lsp-python-ms lsp-intellij lorem-ipsum jtags ipretty intellij-theme htmltagwrap hide-mode-line evil company))
- '(pdf-view-midnight-colors (cons "#d4d4d4" "#1e1e1e"))
+   '(md-readme vterm tide skewer-mode shell-pop rjsx-mode prettier-js prettier org-preview-html multiple-cursors lsp-python-ms lsp-javacomp lsp-java lsp-intellij lorem-ipsum jtags javap-mode javaimp javadoc-lookup ipretty intellij-theme htmltagwrap hide-mode-line gotham-theme evil company))
+ '(pdf-view-midnight-colors (cons "#bbc2cf" "#282c34"))
  '(rustic-ansi-faces
-   ["#1e1e1e" "#D16969" "#579C4C" "#D7BA7D" "#339CDB" "#C586C0" "#85DDFF" "#d4d4d4"])
- '(vc-annotate-background "#1e1e1e")
+   ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
+ '(vc-annotate-background "#282c34")
  '(vc-annotate-color-map
-    (list
-         (cons 20 "#579C4C")
-         (cons 40 "#81a65c")
-         (cons 60 "#acb06c")
-         (cons 80 "#D7BA7D")
-         (cons 100 "#d8ab79")
-         (cons 120 "#d99c76")
-         (cons 140 "#DB8E73")
-         (cons 160 "#d38b8c")
-         (cons 180 "#cc88a6")
-         (cons 200 "#C586C0")
-         (cons 220 "#c97ca3")
-         (cons 240 "#cd7286")
-         (cons 260 "#D16969")
-         (cons 280 "#ba6c6c")
-         (cons 300 "#a37070")
-         (cons 320 "#8d7374")
-         (cons 340 "#37474F")
-         (cons 360 "#37474F")))
+   (list
+     (cons 20 "#98be65")
+     (cons 40 "#b4be6c")
+     (cons 60 "#d0be73")
+     (cons 80 "#ECBE7B")
+     (cons 100 "#e6ab6a")
+     (cons 120 "#e09859")
+     (cons 140 "#da8548")
+     (cons 160 "#d38079")
+     (cons 180 "#cc7cab")
+     (cons 200 "#c678dd")
+     (cons 220 "#d974b7")
+     (cons 240 "#ec7091")
+     (cons 260 "#ff6c6b")
+     (cons 280 "#cf6162")
+     (cons 300 "#9f585a")
+     (cons 320 "#6f4e52")
+     (cons 340 "#5B6268")
+     (cons 360 "#5B6268")))
  '(vc-annotate-very-old-color nil))
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
