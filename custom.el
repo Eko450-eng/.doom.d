4c96ae37068546646e1c45677831780bea24a4b2
Eko450-eng <ekrem@wipdesig.de>
2022-05-11 23:05:49 +0200

    Backup

--- custom.el
+++ custom.el
@@ -1,9 +1,18 @@
+(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
+(setq doom-theme 'nord)
+
+(use-package fira-code-mode
+  :custom (fira-code-mode-disabled-ligatures '("[]" "</" "</>" "#{" "#(" "#_" "#_(" "x")) ;; List of ligatures to turn off
+  :hook prog-mode) ;; Enables fira-code-mode automatically for programming major modes
+
 (setq auto-mode-alist (delete '("\\.ts\\'" . typescript-tsx-mode) auto-mode-alist))
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . rjsx-mode))
 (add-to-list 'auto-mode-alist '("\\.java\\'" . java-mode))
 
-;; Keybinds
-;; Moving windows
+;; (global-set-key (kbd "C-x C-u") 'lsp-java-organize-imports)
+
+(setq js2-mode-show-strict-warnings nil)
+
 (global-set-key (kbd "M-l") 'evil-window-right)
 (global-set-key (kbd "M-h") 'evil-window-left)
 (global-set-key (kbd "M-j") 'evil-window-down)
@@ -19,21 +28,20 @@
 (global-set-key (kbd "M-C-j") 'evil-window-decrease-height)
 (global-set-key (kbd "M-C-k") 'evil-window-increase-height)
 
-(global-set-key (kbd "M-v") 'unbind-key)
-(global-set-key (kbd "M-v") 'split-window-vertically)
 (global-set-key (kbd "M-s") 'unbind-key)
-(global-set-key (kbd "M-s") 'split-window-horizontally)
+(global-set-key (kbd "M-s") 'split-window-vertically)
+(global-set-key (kbd "M-v") 'unbind-key)
+(global-set-key (kbd "M-v") 'split-window-horizontally)
 
-;; Blink the cursor
+(global-set-key (kbd "M-y") 'lsp)
+
 (global-set-key (kbd "M-G") 'unbind-key)
 (global-set-key (kbd "M-G") 'beacon-blink)
-;; Open bookmarks
+
 (global-set-key (kbd "M-c") 'treemacs-switch-workspace)
 
-;; Toggling vterm
 (global-set-key (kbd "s-x") 'vterm-toggle)
 
-;; Open vterm at bottom
 (setq vterm-toggle-fullscreen-p nil)
 (add-to-list 'display-buffer-alist
              '((lambda(bufname _) (with-current-buffer bufname
@@ -42,18 +50,8 @@
                (display-buffer-reuse-window display-buffer-at-bottom)
                (window-height . 0.2)))
 
-;; Organize and import java imports
-;; (global-set-key (kbd "C-x C-u") 'lsp-java-organize-imports)
-
-;; Anything else
-(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
-
-(setq doom-theme 'doom-dark+)
-
-;; Enable packages
 (beacon-mode 1)
 
-;; Org mode
 (setq org-hide-emphasis-markers t)
 
 (font-lock-add-keywords 'org-mode
@@ -62,57 +60,53 @@
 
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
-(use-package fira-code-mode
-  :custom (fira-code-mode-disabled-ligatures '("[]" "</" "#{" "#(" "#_" "#_(" "x")) ;; List of ligatures to turn off
-  :hook prog-mode) ;; Enables fira-code-mode automatically for programming major modes
+(rainbow-mode 1)
 
-(setq js2-mode-show-strict-warnings nil)
-
-;; (use-package tide
-;;   :after (typescript-mode company flycheck)
-;;   :hook ((typescript-mode . tide-setup)
-;;          (typescript-mode . tide-hl-identifier-mode)
-;;          (before-save . tide-format-before-save)))
+(setq)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
-   ["#1e1e1e" "#D16969" "#579C4C" "#D7BA7D" "#339CDB" "#C586C0" "#85DDFF" "#d4d4d4"])
+   ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
  '(custom-safe-themes
    '("e3b2bad7b781a968692759ad12cb6552bc39d7057762eefaf168dbe604ce3a4b" default))
- '(exwm-floating-border-color "#252526")
- '(fci-rule-color "#37474F")
- '(highlight-tail-colors ((("#232a22" "#232a21") . 0) (("#283134" "#243034") . 20)))
- '(jdee-db-active-breakpoint-face-colors (cons "#171F24" "#237AD3"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#171F24" "#579C4C"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#171F24" "#777778"))
- '(objed-cursor-color "#D16969")
- '(pdf-view-midnight-colors (cons "#d4d4d4" "#1e1e1e"))
+ '(exwm-floating-border-color "#191b20")
+ '(fci-rule-color "#5B6268")
+ '(highlight-tail-colors
+   ((("#333a38" "#99bb66" "green")
+     . 0)
+    (("#2b3d48" "#46D9FF" "brightcyan")
+     . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#51afef"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#98be65"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
+ '(objed-cursor-color "#ff6c6b")
+ '(pdf-view-midnight-colors (cons "#bbc2cf" "#282c34"))
  '(rustic-ansi-faces
-   ["#1e1e1e" "#D16969" "#579C4C" "#D7BA7D" "#339CDB" "#C586C0" "#85DDFF" "#d4d4d4"])
- '(vc-annotate-background "#1e1e1e")
+   ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
+ '(vc-annotate-background "#282c34")
  '(vc-annotate-color-map
    (list
-    (cons 20 "#579C4C")
-    (cons 40 "#81a65c")
-    (cons 60 "#acb06c")
-    (cons 80 "#D7BA7D")
-    (cons 100 "#d8ab79")
-    (cons 120 "#d99c76")
-    (cons 140 "#DB8E73")
-    (cons 160 "#d38b8c")
-    (cons 180 "#cc88a6")
-    (cons 200 "#C586C0")
-    (cons 220 "#c97ca3")
-    (cons 240 "#cd7286")
-    (cons 260 "#D16969")
-    (cons 280 "#ba6c6c")
-    (cons 300 "#a37070")
-    (cons 320 "#8d7374")
-    (cons 340 "#37474F")
-    (cons 360 "#37474F")))
+    (cons 20 "#98be65")
+    (cons 40 "#b4be6c")
+    (cons 60 "#d0be73")
+    (cons 80 "#ECBE7B")
+    (cons 100 "#e6ab6a")
+    (cons 120 "#e09859")
+    (cons 140 "#da8548")
+    (cons 160 "#d38079")
+    (cons 180 "#cc7cab")
+    (cons 200 "#c678dd")
+    (cons 220 "#d974b7")
+    (cons 240 "#ec7091")
+    (cons 260 "#ff6c6b")
+    (cons 280 "#cf6162")
+    (cons 300 "#9f585a")
+    (cons 320 "#6f4e52")
+    (cons 340 "#5B6268")
+    (cons 360 "#5B6268")))
  '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
